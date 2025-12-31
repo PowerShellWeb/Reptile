b3ba71e804ddf9ae9e4f0498fe440ba90458d136
StartAutomating <james.brundage@start-automating.com>
2025-12-30 20:03:01 -0800

    feat: Turtle Reptile Example ( Fixes #6 )
    Improving button behavior and adding examples

--- Examples/TurtleShell.html.ps1
+++ Examples/TurtleShell.html.ps1
@@ -3,8 +3,8 @@
 "</head>"
 "<body>"
 "<style>"    
-".repl-input { grid-area: input; }"
-".repl-go { grid-area: go; width: 50%; }"
+".repl-input { grid-area: input; width: 100%; }"
+".repl-go { grid-area: go; width: 50%; text-align: center; }"
 "body { background-color: black; max-width: 100vw; height: 100vh; color: white; }"
 ".outputGrid {
     display: grid; 
@@ -25,15 +25,40 @@
     grid-template-rows: auto auto;
     grid-template-columns: 1fr;
 }"
-".repl-input { width: 100%; }"
+
 "</style>"
 "<div class='repl-input-area'>"
-"<textarea class='repl-input' id='input' spellcheck='false'>"
-'turtle rotate (randomangle) ("flower", "starflower" | Random) fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'
+"<textarea class='repl-input' id='input' rows='3' spellcheck='false'>"
+'turtle rotate (randomangle) (
+    "flower", "starflower", "square", "circle", "rectangle" | random
+) fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'
 "</textarea>"
 "<button class='repl-go' id='go'>Go</button>"
 "</div>"
+"<menu>"
+"<button class='reptile' id='circle' data-input='turtle circle 42 fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Circle</button>"
+"<button class='reptile' id='pie' data-input='turtle rotate (randomangle) pie 42 3 fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Pie</button>"
+"<button class='reptile' id='square' data-input='turtle rotate (randomangle) square 42 fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Square</button>"
+"<button class='reptile' id='rectangle' data-input='turtle rotate (randomangle) rectangle 42 fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Rectangle</button>"
+"<button class='reptile' id='star' data-input='turtle rotate (randomangle) star 42 (5,6,7,8,9 | Get-Random) fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Star</button>"
+"<button class='reptile' id='flower' data-input='turtle rotate (randomangle) flower 42 fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Flower</button>"
+"<button class='reptile' id='starflower' data-input='turtle rotate (randomangle) starflower 42 fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)'>Starflower</button>"
+"<details open><summary>Sectors</summary>"
+@"
+<button class='reptile' id='quadrants' data-input="turtle id quadrants rotate (randomangle) @(
+    'CircleArc',42, 90, 'Rotate', 90 * 4
+) fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)">Quadrants</button>
 
+<button class='reptile' id='sextants' data-input="turtle id sextants rotate (randomangle) @(
+    'CircleArc',42, 60, 'Rotate', 60 * 6
+) fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)">Sextants</button>
+
+<button class='reptile' id='octants' data-input="turtle id quadrants rotate (randomangle) @(
+    'CircleArc',42, 45, 'Rotate', 45 * 8
+) fill (randomcolor) (randomcolor) stroke (randomcolor) (randomcolor)">Octants</button>
+"@
+"</details>"
+"</menu>"
 "<output id='output'></output>"
 "<ol id='output-item-list'>"    
 "</ol>"
@@ -51,7 +76,9 @@ function newShell(input, options = {}) {
     const newListOutput = document.createElement('output') 
     const newListInput = document.createElement('textarea')
     newListInput.setAttribute('spellcheck','false')
+    newListInput.setAttribute('rows','3')
     newListInput.classList.add('repl-input')
+    
     
     const now = new Date()
     newListInput.id = 'input' + now.getTime()
@@ -63,6 +90,7 @@ function newShell(input, options = {}) {
 
     const newGoButton = document.createElement('button') 
     newGoButton.innerText = 'go' 
+    newGoButton.classList.add('repl-go')
     newGoButton.addEventListener('click', go)
     newListDetails.appendChild(newGoButton)
     newListDetails.appendChild(newListOutput)        
@@ -84,39 +112,20 @@ function newShell(input, options = {}) {
     if (
         event?.target?.dataset?.input
     ) {
-        const response = 
-            await fetch(
-                window.location.href,
-                    {method: 'POST',body: event?.target?.dataset?.input}
-            )
-        const responseText = await response.text()
-        
-        if (event?.target?.dataset?.output) {
-            for (const out in [
-                ...document.querySelectorAll(event?.target?.dataset?.output)
-            ]) {
-                out.innerHTML = responseText;
-                out.animate({ scale: ['0%', '100%'] }, 84);
-            }
-        } else {
-            const out = document.createElement('output') 
-            out.innerHTML = responseText;
-            const mainOutput = document.getElementById('output')
-            if (mainOutput?.firstChild) {            
-                mainOutput.insertBefore(out, mainOutput.firstChild)
-                out.animate({ scale: ['0%', '100%'] }, 84);
-            } else if (mainOutput) {
-                mainOutput.appendChild(out)
-                out.animate({ scale: ['0%', '100%'] }, 84);
-            }                        
-        }            
+        inputScript = event?.target?.dataset?.input
+        inputId = newShell(inputScript)
+        const outputId = inputId.replace(/^input/i, 'output')
+        const out = document.getElementById(outputId)
+        const response = await fetch(window.location.href,
+            {method: 'POST',body: inputScript})
+        out.innerHTML = await response.text()
+        out.animate({ scale: ['0%', '100%'] }, 84);
         return
     }
     if (event?.target?.previousSibling?.value && 
         event?.target?.previousSibling?.id.match(/^input/)) {
         inputId = event?.target?.previousSibling?.id
-        const outputId = inputId.replace(/^input/i, 'output')
-    
+        const outputId = inputId.replace(/^input/i, 'output')    
     }
     
     if (! inputScript && inputId == 'input' || ! inputId) {
@@ -144,6 +153,5 @@ function newShell(input, options = {}) {
 }"
 "document.getElementById('go').addEventListener('click', go)"
 "</script>"    
-"<button class='reptile' id='star' data-input='turtle star 42 (4,5,6,7,8,9 | Get-Random) fill (randomcolor) stroke (randomcolor)'>Star</button>"
 "</body>"
 "</html>"
