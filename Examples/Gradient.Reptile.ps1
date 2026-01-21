74dababd204b47cbbdfcadc2778e320c87e719f9
StartAutomating <james.brundage@start-automating.com>
2026-01-21 15:27:06 -0800

    feat: Gradient reptile ( Fixes #9 )
    Adding labels

--- Examples/Gradient.Reptile.ps1
+++ Examples/Gradient.Reptile.ps1
@@ -14,13 +14,16 @@ Reptile -SupportedCommand @(
 <html><head><title>Gradient Reptile</title></head>
 <body>
 <form action='/' method='post'>
-    <input type='color' name='color1' value='#4488ff' />
-    <input type='color' name='color2' value='#224488' />
-    <select name='gradientType'>
+    <input type='color' id='color1' name='color1' value='#4488ff' />
+    <label for='color1'>Color 1</label>
+    <input type='color' id='color2' name='color2' value='#224488' />
+    <label for='color2'>Color 2</label>
+    <select id='gradientType' name='gradientType'>
         <option selected>radial</option>
         <option>linear</option>
         <option>conic</option>
     </select>
+    <label for='gradientType'>Gradient Type</label>
     <input type='hidden' name='command' value="$(
         [Web.HttpUtility]::HtmlAttributeEncode(@'
 "<div style='width:100%;height:100%;background:$(gradient $gradientType $color1 $color2)'></div>"
